b810ba50e0e81321cf4fd49170bcc5ddbade3534
Josue Fernandez <josue.0795@gmail.com>
2021-08-24 21:18:05 -0600

    Added name to Event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -64,6 +64,6 @@ class EventsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def event_params
-      params.require(:event).permit(:event_date, :restaurant_id)
+      params.require(:event).permit(:name, :event_date, :restaurant_id)
     end
 end
