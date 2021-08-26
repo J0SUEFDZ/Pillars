7434be3f0ef52278dfdeedc736fb28620e8c4d17
Josue Fernandez <josue.0795@gmail.com>
2021-08-25 21:46:29 -0600

    Fixed error on delete for polymorphic relations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,4 @@
 class Event < ApplicationRecord
     belongs_to :restaurant
+    has_many :reviews, as: :reviewed, dependent: :destroy
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,9 +1,4 @@
-require 'pry'
-
 class Restaurant < ApplicationRecord
     has_many :events, dependent: :destroy
-
-    def restaurant_events
-        events.collect(&:event_date)
-    end
+    has_many :reviews, as: :reviewed, dependent: :destroy
 end
